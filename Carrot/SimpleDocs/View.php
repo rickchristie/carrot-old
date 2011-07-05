2537771003e9f230a84e97ee328fa726a7377949
Rick Christie <seven.rchristie@gmail.com>
2011-07-06 02:34:38 +0700

    Edited readme.markdown, more work on the docs in Sample

--- Carrot/SimpleDocs/View.php
+++ Carrot/SimpleDocs/View.php
@@ -110,24 +110,25 @@ class View
         	#sidebar { width: 200px; float: left; font-size: 12px; }
         	#sidebar ul { margin: 0 0 23px 0; padding: 0; list-style-type: none; line-height: 1.1; }
         	#sidebar ul li { margin: 9px 0; }
-        	#sidebar a { text-decoration: none; color: #7a7a7a; }
+        	#sidebar a { text-decoration: none; color: #7a7a7a; border: none; }
         	#sidebar a.current { color: #000; }
         	#sidebar a:hover { color: #000; }
-        	#sidebar h3 { margin: 0 0 12px 0; font-size: 14px; }
+        	#sidebar h3 { margin: 0 0 12px 0; font-size: 15px; }
         	#content { width: 540px; margin-left: 230px }
-        	#content ul { margin: 20px 0 20px 0; padding: 0 0 0 0; list-style-type: none; }
-        	#content ul li { margin: 7px 0; padding: 0 0 0 20px; }
+        	#content ul { margin: 20px 0 22px 0; padding: 0 0 0 0; list-style-type: none; }
+        	#content ul li { margin: 8px 0; padding: 0 0 0 40px; background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAPxQTFRF/Pn2+PDo5820tWUXtmca9uziyGYJ/2kE/3UF2E8G/2gG/5UJzJVg+/fz03UI004H01IG6XEI4HoH4ODPz08GuW4ms2ESls6l1mMI797Ow+DKLJVICoMp4loF6VAF+fPu/34FknYquVwI5n4G/40I/v3869bCzZdj2bCISKli1uvbZ7R7FYIy/5EIuWEI71MEwX49+vXx/5oJ/1kF2lUF4e/l028I2+zg6tO8/3YH48WoEIQp0J1rt2og8fjzyFEH/4oH/4MI/4EI+PHrxFsItGMVemgUzujV7drI2HMIE4Ix4vHm/10EuGwj4L+fwHw6/50Iv3o3ypFa////MSaHZAAAAFR0Uk5T//////////////////////////////////////////////////////////////////////////////////////////////////////////////8AU/dy0QAAAI5JREFUeNpczzUWw0AUQ9FvCjOzw8zMzJxo/3tJlbHHKu9R8wiGEW5GqNbCJw4Qi1eWSkeDwTUxyzSlsfYY1Y+udy4UZQB/Nt3o+h4CA1okLWv7pU1/wFnum7f54ZwBlJfnPtmYRAagUqBl88oqA+ApWXsRhw4gfFepfVEHgBh0fwrEhZUPU+Jbd86fAAMAJ31C2sCnrroAAAAASUVORK5CYII=) 18px 0px no-repeat; }
         	h1 { font-size: 25px; margin: 20px 0 20px 0; }
-        	h2 { font-size: 20px; margin: 20px 0 20px 0; }
+        	h2 { font-size: 18px; margin: 20px 0 20px 0; }
         	h1, h2, h3, h4, h5, h6 { line-height: 1.2; font-weight: normal; color: #000; }
         	code { font-family: 'Monaco', 'Consolas', 'Courier', 'Courier New', monospace; font-size: 90%; }
     		pre { font-family: 'Monaco', 'Consolas', 'Courier', 'Courier New', monospace; font-size: 12px; display: block; overflow: auto; background: #edf0f3; border: 1px solid #cbd3db; line-height: 17px; -moz-border-radius: 5px; border-radius: 5px; padding: 15px; margin: 25px 0; }
             p { margin: 18px 0; }
-            blockquote { margin: 18px 0 18px 40px; font-size: 14px; color: #666; }
-            blockquote cite { font-size: 12px }
-            a:link { color: #f13900; }
-    		a:visited { color: #ff5825; }
-    		a:hover { color: #ff843a; }
+            em { font-style: italic }
+            blockquote { margin: 18px 0 20px 40px; font-size: 14px; color: #666; }
+            blockquote cite { font-size: 12px; font-style: normal; }
+            a { color: #d56600; text-decoration: none; }
+            a:visited { color: #b05400; }
+    		a:hover { color: #ff843a; border-bottom: 1px solid #dfb97b; }
     		a:active { color: #f13900; }
         	.clearfix:after { content: "."; display: block; height: 0; clear: both; visibility: hidden; }
             .clearfix { display: inline-block; }
@@ -178,7 +179,7 @@ class View
                 <ul>
                     <?php foreach ($pages as $page): ?>
                     <li><a class="<?php echo $page['class'] ?>" href="<?php echo $page['url'] ?>">
-                        <?php if ($page['class'] == 'current') echo '&rarr;'; ?>
+                        <?php if ($page['class'] == 'current') echo '@'; ?>
                         <?php echo $page['title'] ?></a>
                     </li>
                     <?php endforeach; ?>
@@ -256,7 +257,7 @@ class View
         ob_start();
         ?>
         <h1>Page Does Not Exist</h1>
-        <blockquote>You just keep pushing. You just keep pushing. I made every mistake that could be made. But I just kept pushing. <cite>- Rene Descartes</cite></blockquote>
+        <blockquote><p>You just keep pushing. You just keep pushing. I made every mistake that could be made. But I just kept pushing. <cite>- Rene Descartes</cite></p></blockquote>
         <p>We are sorry, but the page you were looking for does not exist. Maybe the link is broken, or maybe the page has been removed. Whatever it is, you can't access it right now. We humbly ask you to consider the <em>real</em> contents available from the links at the sidebar to the left.</p>
         <?php
         return ob_get_clean();
--- README.markdown
+++ README.markdown
@@ -1,42 +1,46 @@
 Carrot: Simple, Experimental PHP Framework (Unstable)
 =====================================================
 
-Carrot is an experimental framework that is created as a learning project. It quickly grows
-from a CodeIgniter-like framework into another beast of its own. It uses dependency injection
-container to instantiate all of the core classes and it uses anonymous functions heavily. Note
-that it's still very unstable, with many changes to the core coming (and features planned).
+Carrot is yet another PHP framework. It was created as a learning
+project, hence it is experimental in nature. Carrot mandates the
+usage of provider classes to manage dependency injection through
+its dependency injection container (DIC). Carrot's core classes
+includes the front controller, exception handler, DIC, and a
+default router. Carrot is still unstable, with many changes (and
+features) planned.
 
-This document should provide you with some information on how Carrot works. Since Carrot is
-still 0.1, this document may change in time. Detailed documentation is in progress, meanwhile,
-please download the source and play with it if you wanted to know more about Carrot.
+Detailed documentation is in progress, meanwhile, please download
+the source and play with it if you wanted to know more about
+Carrot.
 
 Requirements
 ------------
 
-- PHP 5.3+
-- Apache web server (not tested in other web servers)
+- PHP 5.3+ (Carrot uses namespaces and anonymous functions).
+- Apache2 web server (Carrot is not tested in other web servers).
 
 Framework design goals
 ----------------------
 
-- Create a framework without using the keyword global or static. Fully recognize the
-  dangerous nature of global state and avoid it at all costs.
-- Relying on dependency injection container to manage the dependencies of user classes,
-  thus eliminating the need for a global objects and registry.
-- Fully utilize PHP's new features by refusing to support outdated PHP installations.
-  One of those features are anonymous functions, which are used extensively throughout
-  the framework.
-- Build and make use of decoupled classes, avoid inheritance whenever possible. This,
-  for one, allows the user's controller class to be a plain old PHP object managed by
-  the dependency injection container. You can even pick one of Carrot's core classes
-  and use it as a standalone class.
-- Make the core as small and focused as possible. Carrot's main job is to be the front
-  controller, setting up the dependency injection container, instantiating the user's
-  controller and getting a response from it. It does not know, much less dictate how
-  the controller is getting the response.
-- Allow the user to replace core classes of Carrot using their own class only by
-  implementing an interface as a contract to the front controller, thereby creating
-  an environment that does not disturb the user's programming routine.
-- Continue the development by adding libraries, which are essentially just decoupled
-  classes that are properly namespaced. Each library must not know that it is being
-  used inside a framework.
+- Create a framework without using the keyword global or static.
+  Fully recognize the dangerous nature of global state and avoid
+  it at all costs.
+- Relying on dependency injection container to manage the
+  dependencies of user classes, thus eliminating the need for a
+  global objects and registry.
+- Build and make use of decoupled classes, avoid inheritance
+  whenever possible. This would allow the user's controller class
+  to be a plain old PHP object managed by the dependency injection
+  container.
+- Make the core as small and focused as possible. Carrot's main
+  job is to be the front controller, setting up the dependency
+  injection container, instantiating the user's controller and
+  getting a response from it. It does not know, much less
+  dictate how the controller is getting the response.
+- Allow the user to replace some of the core classes of Carrot
+  using their own class only by implementing an interface as a
+  contract to the core classes.
+- Continue the development by adding libraries, which are
+  essentially just decoupled classes that are properly namespaced.
+  Each library must not know that it is being used inside a
+  framework.
